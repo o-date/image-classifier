ce1c8c7e113f53d0cbb95648f4385ff9308adca3
Shawn Graham <shawn_graham@carleton.ca>
2018-06-19 22:37:40 -0400

    Add files via upload

--- potbot.py
+++ potbot.py
@@ -1,22 +1,16 @@
 from bs4 import BeautifulSoup
 import csv
 import requests
+import re
 
-r = requests.get('http://potsherd.net/atlas/Ware/')
-soup = BeautifulSoup(r.content, "html.parser")
-
-# soup = BeautifulSoup(open("Africa.html"))
-
-# final_link = soup.p.a
-# final_link.decompose()
+r = requests.get('http://potsherd.net/atlas/Ware/ALL')
+soup = BeautifulSoup(r.content, "html5lib")
 
 f = csv.writer(open("output.csv", "w"))
-f.writerow([" ", "Link"])    # Write column headers as the first line
 
 trs = soup.find_all('tr')
-
 for tr in trs:
-    for link in tr.find_all('a'):
+    for link in tr.find_all('a', href=re.compile('^Ware')):
         fulllink = link.get ('href')
         print (fulllink) #print in terminal to verify results
-        f.writerow(["http://potsherd.net/atlas/", fulllink])    # Write column headers as the first line
+        f.writerow(["http://potsherd.net/atlas/" + fulllink])    # Write column headers as the first line
