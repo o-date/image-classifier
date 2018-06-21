f24ad5d63569189f1d9bc16ce4c8ee61fd5ef41e
Shawn Graham <shawn_graham@carleton.ca>
2018-06-21 12:37:13 -0400

    Update potbot.py

--- potbot.py
+++ potbot.py
@@ -10,7 +10,7 @@ f = csv.writer(open("output.csv", "w"))
 
 trs = soup.find_all('tr')
 for tr in trs:
-    for link in tr.find_all('a', href=re.compile('^Ware')):
+    for link in tr.find_all('a'):
         fulllink = link.get ('href')
         print (fulllink) #print in terminal to verify results
         f.writerow(["http://potsherd.net/atlas/" + fulllink])    # Write column headers as the first line
